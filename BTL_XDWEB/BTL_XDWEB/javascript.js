dbd4e41a881feaae3355cd3f4566146c5ed64baa
giang <22010421@st.phenikaa-uni.edu.vn>
2024-05-25 02:02:16 +0700

    chỉnh sửa, update chức năng và thêm javascript + ảnh

---
+++ BTL_XDWEB/BTL_XDWEB/javascript.js
@@ -0,0 +1,23 @@
+const arrowsRight = document.querySelectorAll(".arrow-right");
+const arrowsLeft = document.querySelectorAll(".arrow-left");
+const movieLists = document.querySelectorAll(".list-film");
+
+
+arrowsRight.forEach((arrow, i) =>{
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", ()=>{
+        clickCounter++;
+        if (itemNumber - (6 + clickCounter) >= 0){
+            movieLists[i].style.transform = `translateX(${
+                movieLists[i].computedStyleMap().get("transform")[0].x.value
+            -310}px)`;  
+        } else {
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+        
+    });
+
+    console.log(movieLists[i].querySelectorAll("img").length)
+});
